460b42b3d1f9c88dfa10ee9c99c7147b5fb481b8
acha <tazcha15@gmail.com>
2018-01-31 19:00:49 +0700

    switch case

---
+++ switch-case.js
@@ -0,0 +1,55 @@
+var tanggal = 31
+var bulan = 'Januari'
+var tahun = 1900
+switch(tanggal) {
+  case 1: {console.log(1); break;}
+  case 2: {console.log(2); break;}
+  case 3: {console.log(3); break;}
+  case 4: {console.log(4); break;}
+  case 5: {console.log(5); break;}
+  case 6: {console.log(6); break;}
+  case 7: {console.log(7); break;}
+  case 8: {console.log(8); break;}
+  case 9: {console.log(9); break;}
+  case 10: {console.log(10); break;}
+  case 11:{console.log(11);break;}
+  case 12:{console.log(12); break;}
+  case 13: {console.log(13); break;}
+  case 14: {console.log(14); break;}
+  case 15: {console.log(15); break;}
+  case 16: {console.log(16); break;}
+  case 17: {console.log(17); break;}
+  case 18: {console.log(18); break;}
+  case 19: {console.log(19); break;}
+  case 20: {console.log(10); break;}
+  case 21: {console.log(21); break;}
+  case 22: {console.log(22); break;}
+  case 23: {console.log(23); break;}
+  case 24: {console.log(24); break;}
+  case 25: {console.log(25); break;}
+  case 26: {console.log(26); break;}
+  case 27: {console.log(27); break;}
+  case 28: {console.log(28); break;}
+  case 29: {console.log(29); break;}
+  case 30: {console.log(30); break;}
+  case 31: {console.log(31); break;}
+  default: {console.log('tidak ditemukan'); break;}
+}
+
+switch(bulan) {
+  case 'Januari': {console.log('Januari'); break;}
+  case 'Februari': {console.log('Februari'); break;}
+  case 'Maret': {console.log('Maret'); break;}
+  case 'April': {console.log('April'); break;}
+  case 'Mei': {console.log('Mei'); break;}
+  case'Juni': {console.log('Juni'); break;}
+  case 'Juli': {console.log('Juli'); break;}
+  case 'Agustus': {console.log('Agustus'); break;}
+  case 'September': {console.log('September'); break;}
+  case 'Oktober': {console.log('Oktober'); break;}
+  case 'November':{console.log('November'); break;}
+  case 'Desember':{console.log('Desember'); break;}
+  default: {console.log('tidak ditemukan');break;}
+}
+
+console.log(tanggal + ' ' + bulan + ' ' + tahun)
