79c2da53c489bf85c616992738141b219fcb8d66
acha <tazcha15@gmail.com>
2018-01-31 22:29:17 +0700

    nomor 1

---
+++ looping-exercise.js
@@ -0,0 +1,20 @@
+// 1. Melakukan Looping Menggunakan While
+
+var loopingPertama = 0;
+var counter = 2;
+
+console.log('LOOPING PERTAMA');
+while(loopingPertama < 20) {
+  loopingPertama += counter;
+  console.log(loopingPertama + ' - I love coding');
+}
+
+
+var loopingKedua = 22;
+var counter2 = 2;
+
+console.log('LOOPING KEDUA');
+while(loopingKedua > 2) {
+  loopingKedua -= counter2;
+  console.log(loopingKedua + ' - I will become fullstack developer');
+}
