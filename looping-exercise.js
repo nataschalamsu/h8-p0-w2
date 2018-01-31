f2fc0e97994b231ac86023d39986d42e077028c3
acha <tazcha15@gmail.com>
2018-01-31 22:37:20 +0700

    nomor 2

--- looping-exercise.js
+++ looping-exercise.js
@@ -18,3 +18,16 @@ while(loopingKedua > 2) {
   loopingKedua -= counter2;
   console.log(loopingKedua + ' - I will become fullstack developer');
 }
+
+
+// 2. Melakukan Looping Menggunakan Form
+
+console.log('LOOPING PERTAMA');
+for(var firstLooping = 1; firstLooping <=20; firstLooping ++) {
+  console.log(firstLooping + ' - I love coding');
+}
+
+console.log('LOOPING KEDUA');
+for(var secondLooping =20; secondLooping >= 1; secondLooping --) {
+  console.log(secondLooping + ' - I will become fullstack developer');
+}
