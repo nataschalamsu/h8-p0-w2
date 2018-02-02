df9ec7dfaa0dddba626092c490fca91fee0ce3f7
acha <tazcha15@gmail.com>
2018-02-02 11:03:00 +0700

    nomor 2 dan 3

--- looping-asterisk.js
+++ looping-asterisk.js
@@ -7,3 +7,22 @@ for(var s = 0; s < rows1; s ++) {
 }
 
 // 2. Menyusun Barisan Bintang dengan Nested Looping
+
+var rows1 = 5;
+
+for (var i = 0; i < rows1; i++) {
+  for (var j = rows1; j <= rows1; j++) {
+  }
+  console.log('*'.repeat(j));
+}
+
+
+// 3. M
+
+var rows1 = 5;
+
+for (var i = 0; i < rows1; i++) {
+  for (var j = i; j <= i; j++) {
+  }
+  console.log('*'.repeat(j));
+}
